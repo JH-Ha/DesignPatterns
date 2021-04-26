ed1cc1b3d609d3a58f0a7bc74db3893ffd19e133
Jaehyeong Ha <cubeprince@gmail.com>
2021-04-27 00:57:24 +0900

    change directory of ch3

---
+++ src/ch3/decorator/App.java
@@ -0,0 +1,12 @@
+package ch3.decorator;
+
+public class App {
+	public static void main(String[] args) throws Exception {
+		LowerCaseInputStream lowerCaseInputStream = new LowerCaseInputStream(System.in);
+		int c;
+		while ((c = lowerCaseInputStream.read()) >= 0) {
+			System.out.print((char) c);
+		}
+		lowerCaseInputStream.close();
+	}
+}
