87eab830f2cb1901a2d21bb9071fb5e7dfcad7a6
Jaehyeong Ha <cubeprince@gmail.com>
2021-04-27 20:29:04 +0900

    Simple way to make singleton but not recommended

---
+++ src/ch5/singleton/Main.java
@@ -0,0 +1,10 @@
+package ch5.singleton;
+
+public class Main {
+	public static void main(String[] args) {
+		Singleton singleton = Singleton.getInstance();
+		Singleton singleton2 = Singleton.getInstance();
+
+		System.out.println(singleton == singleton2);
+	}
+}
